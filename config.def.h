a9fb1eeb1757dd1eab878d30a218498e4199f248
Jason Wilkes <notarealdeveloper@gmail.com>
2024-01-02 10:03:30 -0800

    Add gmrun command

--- config.def.h
+++ config.def.h
@@ -59,13 +59,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "gnome-terminal", "--maximize", NULL };
-static const char *fmcmd[]  = { "fm", NULL };
+static const char *fmcmd[]    = { "fm", NULL };
+static const char *gmruncmd[] = { "gmrun", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = fmcmd } },
+	{ MODKEY,                       XK_F2,     spawn,          {.v = gmruncmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
